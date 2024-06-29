23c2c0cb75236753b0ce30a69251ff822f6fa2c1
kovalski21 <113896991+kovalski21@users.noreply.github.com>
2024-06-29 18:02:22 +0300

    Fixed NameError QFont

--- second_win.py
+++ second_win.py
@@ -1,47 +1,5 @@
-# напиши здесь код для второго экрана приложения
-'''from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton, QLabel, QVBoxLayout, QHBoxLayout
-from instr import *
-class TestWin(QWidget):
-    def __init__(self):
-        super().__init__()
-        self.set_appear()
-        self.initUI()
-        self.connects()
-        self.show()
-    def set_appear(self):
-        self.setWindowTitle(txt_title)
-        self.resize(win_width,win_height)
-        self.move(win_x,win_y)
-    def initUI(self):
-        self.btn_next = QPushButton(txt_sendresults)
-        self.btn1 = QPushButton(txt_test1)
-        self.btn2 = QPushButton(txt_test2)
-        self.btn3 = QPushButton(txt_test3)
-        self.timer = QLabel("0.00.00")
-        self.h_line = QHBoxLayout()
-        self.r_line = QVBoxLayout()
-        self.l_line = QVBoxLayout()
-        self.l_line.addWidget(self.btn1)
-        self.l_line.addWidget(self.btn2)
-        self.l_line.addWidget(self.btn3)
-        self.l_line.addWidget(self.btn_next)
-        self.r_line.addWidget(self.timer)
-        self.h_line.addLayout(self.l_line)
-        self.h_line.addLayout(self.r_line)
-        self.setLayout(self.h_line)
-    def connects(self):
-        self.btn_next.clicked.connect(self.next_click)       
-    def next_click(self):
-        self.hide()
-        #self.tw = FinalWin()
-app = QApplication([])
-mw = TestWin()
-app.exec_()'''
-
-
-
-from PyQt5.QtCore import Qt, QTimer, QTime, QLocale
+from PyQt5.QtCore import Qt, QTimer, QTime
+from PyQt5.QtGui import QFont
 from PyQt5.QtWidgets import (
         QApplication, QWidget,
         QHBoxLayout, QVBoxLayout,
@@ -81,7 +39,7 @@ class TestWin(QWidget):
         global time
         time = time.addSecs(-1)
         self.text_timer.setText(time.toString("hh:mm:ss"))
-        #self.text_timer.setFont(QFont("Times", 36, QFont.Bold))
+        self.text_timer.setFont(QFont("Times", 36, QFont.Bold))
         self.text_timer.setStyleSheet("color: rgb(0,0,0)")
         if time.toString("hh:mm:ss") == "00:00:00":
             self.timer.stop()
@@ -89,12 +47,35 @@ class TestWin(QWidget):
         global time
         time = time.addSecs(-1)
         self.text_timer.setText(time.toString("hh:mm:ss")[6:8])
-        #self.text_timer.setFont(QFont("Times",36,QFont.Bold))
+        self.text_timer.setFont(QFont("Times",36,QFont.Bold))
         self.text_timer.setStyleSheet("color: rgb(0,0,0)")
+
+    def timer3Event(self):
+        global time
+        time = time.addSecs(-1)
+        self.text_timer.setText(time.toString("hh:mm:ss"))
+        self.text_timer.setFont(QFont("Times", 36, QFont.Bold))
+        if int(time.toString("hh:mm:ss")[6:8]) >=45:
+            self.text_timer.setStyleSheet("color: rgb(0,255,0)")
+        elif int(time.toString("hh:mm:ss")[6:8]) <= 15:
+            self.text_timer.setStyleSheet("color: rgb(0,255,0)")
+        else:
+            self.text_timer.setStyleSheet("color: rgb(0,0,0)")
+        if time.toString("hh:mm:ss") == "00:00:00":
+            self.timer.stop()
+
+    def timer_test3(self):
+        global time 
+        time = QTime(0,1,0)
+        self.timer = QTimer()
+        self.timer.timeout.connect(self.timer3Event)
+        self.timer.start(1000)
+        
     def connects(self):
         self.btn_next.clicked.connect(self.next_click)
         self.btn_test1.clicked.connect(self.timer_test)
         self.btn_test2.clicked.connect(self.timer_sits)
+        self.btn_test3.clicked.connect(self.timer_test3)
 
     def set_appear(self):
         self.setWindowTitle(txt_title)
